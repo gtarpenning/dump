000c6871a4180ff6caca25f142d09d4558b04152
gtarpenning <gtarpenning@gmail.com>
2023-06-04 12:16:01 -0700

    strict lint

--- cli_v1.py
+++ cli_v1.py
@@ -1,19 +1,22 @@
-import os
-import openai
 import argparse
-import re
 import json
+import os
+import re
+from argparse import Namespace
+from typing import Optional
+
+import openai
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 WHISPER_PROMPT = "Um, well, I sort of did this at 10:00, and also at 1:00 I worked out."
 
 
-def transcribe(filename: str, use_cache=True, verbose=False) -> str:
+def transcribe(filename: str, use_cache: bool = True, verbose: bool = False) -> str:
     """Transcribe an audio file to text."""
     # check if cached
     if use_cache:
-        if f"{filename}.txt" in os.listdir('text'):
+        if f"{filename}.txt" in os.listdir("text"):
             cache_path = f"text/{filename}.txt"
             if verbose:
                 print(f"Returning from cached file: {cache_path}")
@@ -21,23 +24,34 @@ def transcribe(filename: str, use_cache=True, verbose=False) -> str:
 
     # transcribe with whisper
     print(f"Hitting WHISPER API, transcribing: {filename}")  # always print this
-    audio_file= open(f"audio/{filename}.m4a", "rb")
+    audio_file = open(f"audio/{filename}.m4a", "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file, prompt=WHISPER_PROMPT)
+    text: str = str(transcript["text"])
 
     # cache
     with open(f"text/{filename}.txt", "w") as f:
-        f.write(transcript["text"])
+        f.write(text)
 
-    return transcript['text']
+    return text
 
 
-def parse_text_with_chatgpt(paragraph, filename=None, use_cache=True, verbose=False, target=False):
+def parse_text_with_chatgpt(
+    paragraph: str,
+    filename: Optional[str] = None,
+    use_cache: bool = True,
+    verbose: bool = False,
+    target: bool = False,
+) -> str:
     if use_cache and filename is not None:
-        if f"{filename}.json" in os.listdir('tags'):
+        if f"{filename}.json" in os.listdir("tags"):
             if verbose:
                 print("Using cached tags...")
             try:
-                return json.load(open(f"tags/{filename}.json", "r"))
+                tags = json.load(open(f"tags/{filename}.json", "r"))
+                if isinstance(tags, list):
+                    return ",".join(tags)
+                else:
+                    return str(tags)
             except Exception as e:
                 print(f"Error: {e}.  Generating new tags...")
 
@@ -47,58 +61,98 @@ def parse_text_with_chatgpt(paragraph, filename=None, use_cache=True, verbose=Fa
         example2 = """{"unhealthy food", "work", "exercise"}"""
         prompt = f"{preamble}And as an Extractor you return a JSON of relevant succinct tags like this example:\n{example2}\nReturn only the json"
     else:
-        example = "I am medium energy, I have had a 9 productive day, I am a low happiness"
+        example = (
+            "I am medium energy, I have had a 9 productive day, I am a low happiness"
+        )
         preamble = f"You are an extractor. As an extractor you extract user content in this format:\n{example}\n"
         example2 = "{energy : 5, productivity : 9, happiness : 2}"
         prompt = f"{preamble} And as an extractor you return a score bounded between 1-10 that captures the user's sentiment for each category in the following json format: {example2}\n Only return the json value."
-
 
     print("Hitting CHAT API...")  # always print this
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": prompt},
-            {"role": "user", "content": paragraph}
+            {"role": "user", "content": paragraph},
         ],
         temperature=0,
     )
-    content = response['choices'][0]['message']['content']
+    content = response["choices"][0]["message"]["content"]
     print(f"{content=}")
     # regex to extract between two curly braces
-    tags = re.findall(r'\{.*?\}', content)
-    if len(tags) > 0:
-        tags = ",".join([x.replace('"', "").strip() for x in tags[0].split(",")])
+    tag_list = re.findall(r"\{.*?\}", content)
+    tag_str = ""
+    if len(tag_list) > 0:
+        tag_str = ",".join([x.replace('"', "").strip() for x in tag_list[0].split(",")])
     else:
         print("No tags found. Returning empty.")
-        tags = "{}"
+        tag_str = "{}"
 
-    print(f"{tags=}")
+    print(f"{tag_str=}")
 
-    return tags
+    return tag_str
 
 
-def parse():
+def parse() -> Namespace:
     parser = argparse.ArgumentParser(description="Transcribe audio files to text.")
-    parser.add_argument("--path", "-p", type=str, help="path to audio file", default="audio/test-msg.m4a")
-    parser.add_argument("--whisp", "-w", help="overrite whisper transcription", action='store_false', default=True)
-    parser.add_argument("--tag", "-t", help="overrite chatgpt tagging", action='store_false', default=True)
-    parser.add_argument("--verbose", "-v", help="increase output verbosity", action="store_true", default=False)
-    parser.add_argument("--target", "-tg", help="audio transcript is a target", action="store_true", default=False)
-    parser.add_argument("--all", "-a", help="all files in audio dir", action="store_true", default=False)
+    parser.add_argument(
+        "--path",
+        "-p",
+        type=str,
+        help="path to audio file",
+        default="audio/test-msg.m4a",
+    )
+    parser.add_argument(
+        "--whisp",
+        "-w",
+        help="overrite whisper transcription",
+        action="store_false",
+        default=True,
+    )
+    parser.add_argument(
+        "--tag",
+        "-t",
+        help="overrite chatgpt tagging",
+        action="store_false",
+        default=True,
+    )
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        help="increase output verbosity",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--target",
+        "-tg",
+        help="audio transcript is a target",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--all", "-a", help="all files in audio dir", action="store_true", default=False
+    )
     args = parser.parse_args()
     return args
 
 
-def main():
+def main() -> None:
     # TODO(zam): Add --all functionality --> accept iterable input.
     args = parse()
-    name = args.path.split('/')[1].split('.')[0]
+    name = args.path.split("/")[1].split(".")[0]
 
     transcription = transcribe(name, use_cache=args.whisp, verbose=args.verbose)
     if args.verbose:
         print(f"Transcription:\n{transcription}")
 
-    tags = parse_text_with_chatgpt(transcription, name, use_cache=args.tag, verbose=args.verbose, target=args.target)
+    tags = parse_text_with_chatgpt(
+        transcription,
+        name,
+        use_cache=args.tag,
+        verbose=args.verbose,
+        target=args.target,
+    )
     print(f"Tags:\n{tags}")
 
 
--- inference.py
+++ inference.py
@@ -1,44 +1,44 @@
 import json
 import random
+from typing import Dict, List
 
 NUM_EXAMPLES = 50
-TARGET_KEY = {"energy":0, "productivity":1, "happiness":2}
+TARGET_KEY = {"energy": 0, "productivity": 1, "happiness": 2}
 TAG_ADJ_CORPUS = ["very", "low", "high"]
 TAG_WORD_CORPUS = ["exercise", "work", "drinking", "food", "clean"]
 
 
-def make_insights(data: dict) -> dict:
-    """ Longterm, this is a cronjob run daily/weekly/monthly to generate high-value insights """
+def make_insights(data: Dict[str, List[str]]) -> Dict[str, List[str]]:
+    """Longterm, this is a cronjob run daily/weekly/monthly to generate high-value insights"""
 
-    return {
-        "category": "10% increase in energy when you sit"
-    }
+    return {"category": ["10% increase in energy when you sit"]}
 
 
-def make_fake_target(num_vals: int = NUM_EXAMPLES):
+def make_fake_target(num_vals: int = NUM_EXAMPLES) -> List[List[int]]:
     ret = []
     for i in range(num_vals):
-        ret.append([random.randint(1,10) for i in range(len(TARGET_KEY))])
+        ret.append([random.randint(1, 10) for i in range(len(TARGET_KEY))])
     return ret
 
 
-def make_fake_input(num_vals: int = NUM_EXAMPLES):
+def make_fake_input(num_vals: int = NUM_EXAMPLES) -> List[str]:
     corp = []
     for i in range(num_vals):
         if random.random() < 0.5:
-            corp += [f"{random.choice(TAG_ADJ_CORPUS)} {random.choice(TAG_WORD_CORPUS)}"]
+            corp += [
+                f"{random.choice(TAG_ADJ_CORPUS)} {random.choice(TAG_WORD_CORPUS)}"
+            ]
         else:
             corp += [f"{random.choice(TAG_WORD_CORPUS)}"]
     return corp
 
 
-
-def get_tags():
+def get_tags() -> None:
     """Extract tags with a given prefix from the tags/ dir"""
     pass
 
 
-def main():
+def main() -> None:
     """testing"""
 
     # load test data
@@ -46,10 +46,7 @@ def main():
         test_data = json.load(open("data/test_data.json", "r"))
     except FileNotFoundError:
         print("No test data found, generating...")
-        test_data = {
-            "input": make_fake_input(),
-            "target": make_fake_target()
-        }
+        test_data = {"input": make_fake_input(), "target": make_fake_target()}
         json.dump(test_data, open("data/test_data.json", "w"))
 
     make_insights(test_data)
