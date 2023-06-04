934dbef77ace23b6a3c60144cba3e19a996003de
gtarpenning <gtarpenning@gmail.com>
2023-06-04 11:49:43 -0700

    pass lint?

--- inference.py
+++ inference.py
@@ -1,14 +1,10 @@
-import glob
 import json
-import numpy as np
 import random
-import sklearn
 
 NUM_EXAMPLES = 50
 TARGET_KEY = {"energy":0, "productivity":1, "happiness":2}
 TAG_ADJ_CORPUS = ["very", "low", "high"]
 TAG_WORD_CORPUS = ["exercise", "work", "drinking", "food", "clean"]
-TAG_CORPUS = [f"{TAG_ADJ_CORPUS[random.randint(0,len(TAG_ADJ_CORPUS))]} {TAG_WORD_CORPUS[random.randint(TAG_WORD_CORPUS)]}" for i in range(NUM_EXAMPLES) if rand.random(0,1) < 0.5 else f"{TAG_WORD_CORPUS[random.randint(0,len(TAG_WORD_CORPUS))]}"]
 
 
 def make_insights(data: dict) -> dict:
@@ -19,21 +15,21 @@ def make_insights(data: dict) -> dict:
     }
 
 
-def make_fake_target(num_vals = NUM_EXAMPLES):
+def make_fake_target(num_vals: int = NUM_EXAMPLES):
     ret = []
     for i in range(num_vals):
-        ret.append([random.randint(1,10) for i in range(len(TARGET_KEY.keys()))])
+        ret.append([random.randint(1,10) for i in range(len(TARGET_KEY))])
     return ret
 
-def make_fake_input(num_vals = 50):
-    a,w = len(TAG_ADJ_CORPUS), len(TAG_WORD_CORPUS)
-    ret = []
+
+def make_fake_input(num_vals: int = NUM_EXAMPLES):
+    corp = []
     for i in range(num_vals):
-        if random.random(0,1) < 0.5:
-            ret.append(f"{TAG_ADJ_CORPUS[random.randint(0,a)]} {TAG_WORD_CORPUS[random.randint(0,w)]}")
+        if random.random() < 0.5:
+            corp += [f"{random.choice(TAG_ADJ_CORPUS)} {random.choice(TAG_WORD_CORPUS)}"]
         else:
-            ret.append(f"{TAG_WORD_CORPUS[random.randint(0,w)]}")
-    return ret
+            corp += [f"{random.choice(TAG_WORD_CORPUS)}"]
+    return corp
 
 
 
@@ -46,7 +42,16 @@ def main():
     """testing"""
 
     # load test data
-    test_data = json.load(open("data/test_data.json", "r"))
+    try:
+        test_data = json.load(open("data/test_data.json", "r"))
+    except FileNotFoundError:
+        print("No test data found, generating...")
+        test_data = {
+            "input": make_fake_input(),
+            "target": make_fake_target()
+        }
+        json.dump(test_data, open("data/test_data.json", "w"))
+
     make_insights(test_data)
 
 
