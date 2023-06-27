4d544d1e8dfdbbcdde45039751016d732aecadee
Jonathan Morales <jonmor@stanford.edu>
2023-06-27 16:32:56 -0700

    app : directory cleanup

--- app/dump2/dump2/Models/Date.swift
+++ app/dump2/dump2/Models/Date.swift
@@ -7,24 +7,18 @@
 
 import Foundation
 
-extension Date
-{
-    func toString(dateFormat format: String ) -> String {
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = format
-        return dateFormatter.string(from: self)
-        
-    }
-
+extension Date {
+  func toString(dateFormat format: String) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = format
+    return dateFormatter.string(from: self)
+  }
 }
 
 extension VoiceViewModel {
-    func covertSecToMinAndHour(seconds : Int) -> String{
-        
-        let (_,m,s) = (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
-        let sec : String = s < 10 ? "0\(s)" : "\(s)"
-        return "\(m):\(sec)"
-        
-    }
+  func covertSecToMinAndHour(seconds: Int) -> String {
+    let (_, m, s) = (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
+    let sec: String = s < 10 ? "0\(s)" : "\(s)"
+    return "\(m):\(sec)"
+  }
 }
--- app/dump2/dump2/Models/Recording.swift
+++ app/dump2/dump2/Models/Recording.swift
@@ -7,10 +7,8 @@
 
 import Foundation
 
-struct Recording : Equatable {
-    
-    let fileURL : URL
-    let createdAt : Date
-    var isPlaying : Bool
-    
+struct Recording: Equatable {
+  let fileURL: URL
+  let createdAt: Date
+  var isPlaying: Bool
 }
